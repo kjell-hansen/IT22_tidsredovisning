85493b3afc78c7507ee93171e63a325e82f72046
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2024-02-01 12:55:04 +0200

    Allt fungerar!

--- db/tidrapportering.sql
+++ db/tidrapportering.sql
@@ -12,19 +12,28 @@ MySQL - 8.0.28 : Database - tidrapportering
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`tidrapportering` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_swedish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
 /*Table structure for table `aktiviteter` */
+
+DROP TABLE IF EXISTS `aktiviteter`;
 
 CREATE TABLE `aktiviteter` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `Namn` varchar(20) COLLATE utf8_swedish_ci NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_swedish_ci;
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `UIX_Namn` (`Namn`)
+) ENGINE=InnoDB AUTO_INCREMENT=584 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_swedish_ci;
 
 /*Data for the table `aktiviteter` */
 
+insert  into `aktiviteter`(`ID`,`Namn`) values 
+(3,'Chillat'),
+(2,'Halvsovit till HBO'),
+(4,'Hoppsan'),
+(1,'Slötittat på Netflix');
+
 /*Table structure for table `uppgifter` */
+
+DROP TABLE IF EXISTS `uppgifter`;
 
 CREATE TABLE `uppgifter` (
   `ID` int NOT NULL AUTO_INCREMENT,
@@ -35,9 +44,18 @@ CREATE TABLE `uppgifter` (
   PRIMARY KEY (`ID`),
   KEY `AktivitetID` (`AktivitetID`),
   CONSTRAINT `uppgifter_ibfk_1` FOREIGN KEY (`AktivitetID`) REFERENCES `aktiviteter` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=142 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_swedish_ci;
 
 /*Data for the table `uppgifter` */
+
+insert  into `uppgifter`(`ID`,`Datum`,`Tid`,`Beskrivning`,`AktivitetID`) values 
+(1,'2024-01-02','01:00:00','Här skriver jag en lång text',1),
+(2,'2023-12-31','05:00:00','Firat nyår',2),
+(4,'2024-01-11','05:30:00','Gjort något viktigt',3),
+(5,'2024-01-16','01:45:00','Hissat flagga',1),
+(6,'2024-01-02','01:20:00','Gissat fel',1),
+(8,'2024-01-05','01:30:00','',1),
+(9,'2024-01-05','01:30:00','',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
